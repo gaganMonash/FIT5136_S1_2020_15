868d1f049cd7715528850412a596b49912682f2a
tver0005 <tver0005@student.monash.edu>
2020-06-06 03:49:14 +1000

    added model Qualification, sel criteria, shuttle

---
+++ MissionToMars/src/main/java/gov/iscc/MissionToMars/model/SelectionCriteria.java
@@ -0,0 +1,15 @@
+package gov.iscc.MissionToMars.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class SelectionCriteria {
+
+    String age;
+    String qualification;
+    String healthRecord;
+
+}
